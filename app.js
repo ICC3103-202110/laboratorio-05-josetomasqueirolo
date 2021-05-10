07b7dfa8f090cb15dfe611da8da82f20b1d5ae39
jostomasqueirolo <jtqueirolo@miuandes.cl>
2021-05-10 18:48:37 -0400

    Creación archivos

---
+++ app.js
@@ -0,0 +1,13 @@
+
+//PARA CREAR TABLA
+
+const { printTable } = require('console-table-printer');
+
+//Create a table
+const testCases = [
+  { index: 3, text: 'I would like some gelb bananen bitte', value: 100 },
+  { index: 4, text: 'I hope batch update is working', value: 300 },
+];
+
+//print
+printTable(testCases);
