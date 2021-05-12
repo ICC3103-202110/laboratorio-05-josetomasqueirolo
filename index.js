4ad65db2b9c0fbf2e44eebc2245dbbf62acf67c4
jostomasqueirolo <jtqueirolo@miuandes.cl>
2021-05-12 12:55:38 -0400

    Procesando

---
+++ index.js
@@ -0,0 +1,11 @@
+const {update} = require("./update")
+const {view} = require("./view")
+const {app} = require("./app")
+const {initModel} = require("./model")
+
+const state = {
+    model: initModel
+    //currentView: view(initModel)
+}
+
+//app(state, update, view)
