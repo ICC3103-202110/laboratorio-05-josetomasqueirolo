775e795d0ee7c53b0336a3948e182a73f14aafe0
jostomasqueirolo <jtqueirolo@miuandes.cl>
2021-05-13 21:39:57 -0400

    Haciendo funcion para preguntar

--- view2.js
+++ view2.js
@@ -1,5 +1,6 @@
 const figlet = require("figlet")
 const chalk = require("chalk")
+const inquirer = require("inquirer")
 
 function getTitle(){
     return chalk.green(
@@ -14,7 +15,35 @@ function getTitle(){
 }
 
 function getTable(model){
-    
+    const {billAmmount} = model
+    const {percentage} = model
+    const {tip} = model
+    const {total} = model
+    return [
+        {
+            "Bill Ammount": billAmmount
+        }
+    ]
+}
+
+function valueInput(model){
+    const {input} = model
+    const message = "Bill ammount?"
+    return inquirer.prompt([
+        {
+            name: "billAmmount",
+            type: "input",
+            message: message,
+            default: input,
+            validate: function(value){
+                if(value>0){
+                    return true
+                } else {
+                    return "Enter a valid number"
+                }
+            }
+        }
+    ])
 }
 
 module.exports = {
