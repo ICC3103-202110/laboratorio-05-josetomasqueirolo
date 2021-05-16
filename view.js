dc8bccc07c9d7dedb75035b98ffdfd281516ab39
jostomasqueirolo <jtqueirolo@miuandes.cl>
2021-05-16 19:37:32 -0400

    Función inputForm hecha

--- view.js
+++ view.js
@@ -1,11 +1,12 @@
 const {printTable} = require("console-table-printer")
 const figlet = require('figlet')
 const chalk = require('chalk')
+const inquirer = require('inquirer')
 
 
-function getTable(billAmount, percentage){
+function getTable(billAmount, percentage, tip, total){
     const table = [
-    { "Bill Amount": billAmount, "Tip (%)": percentage, "Tip": billAmount/percentage, "Total": billAmount+billAmount/percentage}
+    { "Bill Amount": billAmount, "Tip (%)": percentage, "Tip": tip, "Total": total}
   ];
   
   printTable(table);
@@ -23,7 +24,44 @@ function getTitle(){
     )
 }
 
+
+function inputForm(){
+    const message1 = 'Bill amount?'
+    const message2 = "Tip percentage?"
+    return inquirer.prompt([
+        {
+            name: 'billAmmount',
+            type: 'input',
+            message: message1,
+            validate: function(value){
+                if(value>0){
+                    return true
+                } else {
+                    return 'Enter a valid number'
+                }
+            }
+        },
+        {
+            name: 'percentage',
+            type: 'input',
+            message: message2,
+            validate: function(value){
+                if(value>0 || value<=100){
+                    return true
+                } else {
+                    return 'Enter a valid number'
+                }
+            }
+        }
+    ])
+}
+
+
+//getTable(1000, 10, 100, 1100)
+inputForm()
+
 module.exports = {
     getTable,
-    getTitle
+    getTitle,
+    inputForm
 }
